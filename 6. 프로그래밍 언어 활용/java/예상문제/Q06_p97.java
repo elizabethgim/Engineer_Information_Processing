128b5845343d29234e85e807f277b316abda3e79
elizabethgim <yujingim@kookmin.ac.kr>
2022-10-10 22:24:30 +0900

    6-97쪽 예상문제 6번

---
+++ 6. 프로그래밍 언어 활용/java/예상문제/Q06_p97.java
@@ -0,0 +1,9 @@
+public class Q06_p97 {
+    public static void main(String[] args){
+        for(int m=0;m<10;m++){
+            if (m%2==0)
+                continue;
+            System.out.print(m);
+        }
+    }
+}
